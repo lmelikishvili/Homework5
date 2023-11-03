cf4b623c4bf6ca6b252235a6e697fbb6c88a2270
lmelikishvili <levanimelikishvili@gmail.com>
2023-11-04 00:49:47 +0400

    before_regStep2

--- app/src/main/java/com/example/homework5/LoginActivity.kt
+++ app/src/main/java/com/example/homework5/LoginActivity.kt
@@ -3,20 +3,44 @@ package com.example.homework5
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.homework5.databinding.ActivityLoginBinding
 import com.example.homework5.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.btnBack.setOnClickListener {
             val intent = Intent(applicationContext,MainActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+
+            val email = binding.etEmail.text.toString()
+            val pass = binding.etPassw.text.toString()
+            if (email.isNotEmpty() && pass.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        Toast.makeText(this, "Login Succesfull!!", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(applicationContext,MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Emplty Fields Are Not Allowed !!", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
     }
--- app/src/main/java/com/example/homework5/RegisterActivity.kt
+++ app/src/main/java/com/example/homework5/RegisterActivity.kt
@@ -3,16 +3,20 @@ package com.example.homework5
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageButton
 import com.example.homework5.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
-
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.btnBack.setOnClickListener(){
             val intent = Intent(applicationContext,MainActivity::class.java)
@@ -20,8 +24,20 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         binding.btnNext.setOnClickListener(){
-            val intent = Intent(applicationContext,RegisterActivity2::class.java)
-            startActivity(intent)
+            val email = binding.etEmail.text.toString()
+            val pass = binding.etPassw.text.toString()
+            if (email.isNotEmpty() && pass.isNotEmpty()){
+                firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        val intent = Intent(applicationContext,LoginActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Emplty Fields Are Not Allowed !!", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
